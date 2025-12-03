f1b620f9b3e1e44cb48ea6c621748f7b570301ed
nedhmn <hermannned@gmail.com>
2025-12-02 19:24:11 -0500

    refactor: config

--- rpemail/config.py
+++ rpemail/config.py
@@ -1,5 +1,7 @@
-from pydantic import Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Annotated
+
+from pydantic import Field, field_validator
+from pydantic_settings import BaseSettings, NoDecode, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -13,10 +15,14 @@ class Settings(BaseSettings):
 
     # To update
     template_name: str = Field(default="signature_v1.html")
-    users: list[str] = Field(
-        default=["nhermann@returnpolicystays.com"],
-        description="List of user emails to update signatures for",
+    users: Annotated[list[str], NoDecode] = Field(
+        ..., description="List of user emails to update signatures for"
     )
+
+    @field_validator("users", mode="before")
+    @classmethod
+    def parse_csv_to_list(cls, emails: str) -> list[str]:
+        return list(emails.split(","))
 
 
 settings = Settings()
